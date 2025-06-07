f36053d872f4c2ca7a20e5a01630a119a2ed7a28
vishcloud <pimpalevishal@gmail.com>
2025-06-07 19:54:14 +0000

    Add ECS execution role and assign to task definitions

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -8,6 +8,7 @@ resource "aws_ecs_task_definition" "backend" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
 
   container_definitions = jsonencode([{
     name      = "backend"
@@ -26,6 +27,7 @@ resource "aws_ecs_task_definition" "frontend" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
 
   container_definitions = jsonencode([{
     name      = "frontend"
