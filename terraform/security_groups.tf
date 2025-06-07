13bb1d2f35b47d31fdbb312de3bf78fdcdfa718e
vishcloud <pimpalevishal@gmail.com>
2025-06-07 19:41:54 +0000

    Fix: Add ALB security group for Terraform

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -1,10 +1,9 @@
 resource "aws_security_group" "alb" {
   name        = "${var.project_name}-alb-sg"
-  description = "Security group for the ALB"
+  description = "Allow HTTP access to ALB"
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    description = "Allow HTTP traffic"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -12,10 +11,13 @@ resource "aws_security_group" "alb" {
   }
 
   egress {
-    description = "Allow all outbound traffic"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-alb-sg"
   }
 }
